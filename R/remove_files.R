df38e543962a8c3a9b182c1f52d98f4f16249bf7
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-28 13:30:18 +0100

    Examples run locally

--- R/remove_files.R
+++ R/remove_files.R
@@ -6,17 +6,13 @@
 #'   beastier:::remove_files("abs.ent")
 #'
 #'   # Create a file
-#'   fasta_filename <- "remove_files.fas"
-#'   beastier:::create_random_fasta(
-#'     n_taxa = 5,
-#'     sequence_length = 10,
-#'     fasta_filename = fasta_filename
-#'   )
-#'   testit::assert(file.exists(fasta_filename))
+#'   filename <- "pres.ent"
+#'   file.copy(from = get_path("anthus_2_4.xml"), to = filename)
+#'   testit::assert(file.exists(filename))
 #'
 #'   # Can safely delete a mix of present and absent files
-#'   beastier:::remove_files(c("abs.ent", fasta_filename))
-#'   testit::assert(!file.exists(fasta_filename))
+#'   beastier:::remove_files(c("abs.ent", filename))
+#'   testit::assert(!file.exists(filename))
 #' @author Richel J.C. Bilderbeek
 remove_files <- function(filenames) {
   for (filename in filenames) {
