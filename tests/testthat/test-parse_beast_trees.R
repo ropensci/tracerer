65791f781a93a51ff3c07e16c91ee4c083e1afb0
richelbilderbeek <richel@richelbilderbeek.nl>
2018-02-01 15:20:12 +0100

    Issue #4

--- tests/testthat/test-parse_beast_trees.R
+++ tests/testthat/test-parse_beast_trees.R
@@ -7,6 +7,18 @@ test_that("use", {
   expect_true(is_trees_posterior(posterior))
 })
 
+
+test_that("issue 4", {
+
+  filename <- get_path("unplottable_anthus_aco.trees")
+  trees <- parse_beast_trees(filename)
+  class(trees) <- "multiPhylo"
+  testthat::expect_silent(
+    phangorn::densiTree(trees)
+  )
+})
+
+
 test_that("abuse", {
 
   testthat::expect_error(
