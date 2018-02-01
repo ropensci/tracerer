3ae9b1edb9acbe06ed2aecbb7885e9d584f20fe6
richelbilderbeek <richel@richelbilderbeek.nl>
2018-02-01 11:20:00 +0100

    calc_geom_mean works

--- R/calc_geom_mean.R
+++ R/calc_geom_mean.R
@@ -1,6 +1,9 @@
 #' Calculate the geometric mean
 #' @param values a numeric vector of values
+#' @return returns the geometrics mean if all values are at least zero,
+#'   else returns NA
 #' @author Richel J.C. Bilderbeek
 calc_geom_mean <- function(values) {
+  if (any(values < 0.0)) return(NA)
   exp(mean(log(values)))
 }
--- tests/testthat/test-calc_geom_mean.R
+++ tests/testthat/test-calc_geom_mean.R
@@ -2,7 +2,13 @@ context("calc_geom_mean")
 
 test_that("use", {
 
-    calc_geom_mean(c(1,2,3))
+  estimates <- parse_beast_log(get_path("beast2_example_output.log"))
+  trace <- remove_burn_ins(estimates, burn_in_fraction = 0.1)
 
+  result <- "nonsense"
+  testthat::expect_silent(
+    result <- tracerer:::calc_geom_mean(values = trace$posterior)
+  )
+  testthat::expect_true(is.na(result))
 
 })
