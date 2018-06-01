f2ac6e88963a40b0143353ba6028a6a93c91467e
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-01 12:49:23 +0200

    Add package documentation, thanks @bjoelle

---
+++ R/tracerer.R
@@ -0,0 +1,30 @@
+#' tracerer: A package to parse BEAST2 output files.
+#'
+#' 'tracerer' allows to parse BEAST2 input files, using
+#' an R interface. 'tracerer' closely follows the functionality
+#' of Tracer, a GUI tool bundled with BEAST and BEAST2,
+#' including its default settings.
+#'
+#' @seealso
+#' These are packages associated with 'tracerer':
+#' \itemize{
+#'   \item{
+#'     The package \link[beautier]{beautier} can create
+#'     BEAST2 input files from R
+#'   }
+#'   \item{
+#'     The package \link[beastier]{beastier} can run
+#'     BEAST2 from R
+#'   }
+#'   \item{
+#'     The package \link[babette]{babette} combines the
+#'     functionality of \link[beautier]{beautier},
+#'     \link[beastier]{beastier} and \link[tracerer]{tracerer}
+#'     into a single workflow
+#'   }
+#' }
+#' @docType package
+#' @name tracerer
+NULL
+
+
