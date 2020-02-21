a57341d5fa9d199efa025811007ee8e18a7b4a6c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-21 14:16:40 +0100

    Use 'message' instead of 'print'

--- R/is_trees_file.R
+++ R/is_trees_file.R
@@ -34,7 +34,7 @@ is_trees_file <- function(
   # BEAST2 saves as 'End;', where ape saves as 'END;'
   if (last_line != "End;" && last_line != "END;") {
     if (verbose) {
-        print("last line should be 'End;'")
+        message("last line should be 'End;'")
     }
     return(FALSE)
   }
