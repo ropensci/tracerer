c7ce2d072498ff9206668f12e2ca20ffae9fc51f
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-29 17:55:55 +0200

    Fix style, thanks @lintr-bot

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,4 +3,7 @@ library(tracerer)
 
 test_check("tracerer")
 
-testthat::expect_equal(0, length(list.files(rappdirs::user_cache_dir(appname = "tracerer"))))
+testthat::expect_equal(
+  0,
+  length(list.files(rappdirs::user_cache_dir(appname = "tracerer")))
+)
