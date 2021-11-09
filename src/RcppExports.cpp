72f58a2d3da01d741997ed9d97f29dc72ab78b27
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-09 15:58:09 +0100

    Update Roxygen and src/RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // calc_act_cpp
 double calc_act_cpp(const std::vector<double>& sample, const int sample_interval);
 RcppExport SEXP _tracerer_calc_act_cpp(SEXP sampleSEXP, SEXP sample_intervalSEXP) {
